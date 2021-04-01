f20c9be1434ef8fc2d7de97308003cd5fdb4df71
Jag <jagdhillon077@gmail.com>
2021-04-01 15:51:31 -0700

    Added README.md file Tested node.js code

--- blockChain.js
+++ blockChain.js
@@ -1,4 +1,14 @@
 
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log('got a request!');
+    response.write('do it again?');
+    response.end();
+});
+
+server.listen(3000);
+
 
 class Block{
     constructor(index, timestamp, data, previousHash = "") {
@@ -7,9 +17,5 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = ""
-    }
-
-    calculateHash() {
-
     }
 }
